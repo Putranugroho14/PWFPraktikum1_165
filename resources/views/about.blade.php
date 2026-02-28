9236321c34dc20be3b6949e9e751c4b042bee45d
Putranugroho14 <putranugroho61@gmail.com>
2026-02-28 15:48:38 +0700

    Pertemuan 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
+            {{ __('About') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-[#111827] overflow-hidden shadow-sm sm:rounded-lg border border-gray-800">
+                <div class="p-6 text-gray-200">
+                    <div class="space-y-4">
+                        <p><span class="font-bold">Nama :</span> Putra Nugroho</p>
+                        <p><span class="font-bold">NIM :</span> 20230140165</p>
+                        <p><span class="font-bold">Program Studi :</span> Teknologi Informasi</p>
+                        <p><span class="font-bold">Hobi :</span> Makan</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/components/primary-button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-6 py-2 bg-white border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-gray-200 focus:bg-gray-200 active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</button>
