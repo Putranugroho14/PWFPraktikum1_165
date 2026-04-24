357c6554312fb78806d7dd48465a1083863a7575
Putranugroho14 <putranugroho61@gmail.com>
2026-04-24 22:23:12 +0700

    P7

---
+++ app/View/Components/DeleteProduct.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class DeleteProduct extends Component
+{
+    public $url;
+    public $name;
+
+    public function __construct($url, $name = 'Product')
+    {
+        $this->url = $url;
+        $this->name = $name;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.delete-product');
+    }
+}
